6e1986c113ec686ad722378567ffa0ea973039a2
sarah <skg@neofilm.us>
2023-01-04 15:51:05 -0500

    Debug errors, repair broken syntax & code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+/* 
+1. fix the syntax error 'Expected corresponding JSX closing tag for <h1>'
+2. print current year to the screen instead of the expression 'new Date().getFullYear()'
+3. print the value of the firstName variable to the screen instead of the name of the variable
+4. make the comment an actual JSX comment
+5. add the 'red' css className attribute to the h3 element
+*/
+
 function App() {
-  const firstName = "Adda";
+  const firstName = "Sarah";
   return (
     <div>
-      <h1>Buggy Code Challenge<h1>
-      <p>This year is: new Date().getFullYear()</p>
+      <h1>Buggy Code Challenge</h1>
+      <p>This year is: {new Date().getFullYear()}</p>
       <p>firstName is my favorite crusher of skillz!</p>
-      // this comment shouldn't appear on the screen, just in the code
-      <h3>Make me colorful!</h3>
+      {/* this comment shouldn't appear on the screen, just in the code */}
+      <h3 className="red">Make me colorful!</h3>
     </div>
   );
 }
